53c72e7eccd5de2d9ed32b42aa92a8187fdc33be
GibletsofJesus <thecraggen@hotmail.co.uk>
2017-03-26 23:26:59 +0100

    Made the splash UI sick as fuck Can now do that 3D camera thing like in melee Highscores are slowly becoming a thing

--- Assets/scripts/UI/splashScreenControls.cs
+++ Assets/scripts/UI/splashScreenControls.cs
@@ -17,8 +17,13 @@ public class splashScreenControls : MonoBehaviour
 	[SerializeField]
 	flash startPrompt;
 	[SerializeField]
-	Image Arrow;
+	Image MainMenuArrow;
 	int menuIndex;
+	[Header("Leaderboards")]
+	[SerializeField]
+	Image LeaderboardsImage;
+	[SerializeField]
+	Text LeaderboardRanks, LeaderboardNames, LeaderboardScores;
 
 	MenuState state = MenuState.idle;
 
@@ -26,18 +31,32 @@ public class splashScreenControls : MonoBehaviour
 	{
 		idle,
 		start,
+		leaderboards,
 		main}
 	;
+
+	Vector3 defaultPos;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		defaultPos = Camera.main.transform.position;
 	}
 
 	public void AllowStart ()
 	{
 		state = MenuState.start;
+	}
+
+	void Update ()
+	{
+		//Melee type camera
+		float v = Input.GetAxis("RSVertical");
+		float h = Input.GetAxis("RSHorizontal");
+
+		Camera.main.transform.localPosition = defaultPos +
+		Camera.main.transform.right * h * 0.15f +
+		Vector3.up * v * 0.15f;
 	}
 
 	// Update is called once per frame
@@ -50,14 +69,14 @@ public class splashScreenControls : MonoBehaviour
 					startPrompt.GetComponent<SpriteRenderer>().enabled = false;
 					mainMenu [0].gameObject.SetActive(true);
 					state = MenuState.main;
-					Arrow.gameObject.SetActive(true);
+					MainMenuArrow.gameObject.SetActive(true);
 				}
 				break;
 			case MenuState.main:
 				menuMoveCD = menuMoveCD > 0 ? menuMoveCD - Time.deltaTime : 0;
 
 				#region menu up/down
-				float v = Input.GetAxis("Vertical");
+				float v = CrossPlatformInputManager.GetAxis("Vertical");
 				if (Mathf.Abs(v) > 0 && menuMoveCD == 0) {
 
 					if (v > 0) {
@@ -72,7 +91,7 @@ public class splashScreenControls : MonoBehaviour
 						menuMoveCD = timePushing > 1 ? 0.05f : 0.15f;
 						menuIndex = menuIndex < 3 ? menuIndex + 1 : 0;
 					}
-					Arrow.rectTransform.anchoredPosition = new Vector2 (-90, -28 - (menuIndex * 15) - 0.2f);
+					MainMenuArrow.rectTransform.anchoredPosition = new Vector3 (-198, -20 - (menuIndex * 40) - 0.15f, 15);
 				}
 				prevV = v;
 				#endregion
@@ -96,6 +115,8 @@ public class splashScreenControls : MonoBehaviour
 							mainMenu [2].GetComponent<flash>().enabled = true;
 							SoundManager.instance.playSound(blips [1],1,1);
 							//Show high scores table
+							LeaderboardsImage.gameObject.SetActive(true);
+							StartCoroutine(Leaderboard(true));
 							break;
 						case 3:
 							mainMenu [3].GetComponent<flash>().enabled = true;
@@ -107,7 +128,130 @@ public class splashScreenControls : MonoBehaviour
 				#endregion
 
 				break;
+			case MenuState.leaderboards:
+				if (CrossPlatformInputManager.GetButton("Jump")) {
+					state = MenuState.idle;
+					StartCoroutine(Leaderboard(false));
+					SoundManager.instance.playSound(blips [2],1,1);
+				}
+				break;
 		}
+	}
+
+	IEnumerator Leaderboard (bool open)
+	{
+		if (!open) {
+			LeaderboardNames.gameObject.SetActive(false);
+			LeaderboardRanks.gameObject.SetActive(false);
+			LeaderboardScores.gameObject.SetActive(false);
+		}
+
+		float lerpy = 0;
+		while (lerpy < 1) {
+			lerpy += Time.deltaTime;
+			LeaderboardsImage.rectTransform.sizeDelta = new Vector2 (
+				Mathf.Lerp(0,114,(open ? lerpy : 1 - lerpy) * 2),
+				Mathf.Lerp(4,98,((open ? lerpy : 1 - lerpy) * 2) - 1));
+			yield return new WaitForEndOfFrame ();
+		}
+		mainMenu [2].GetComponent<flash>().enabled = false;
+		mainMenu [2].enabled = true;
+		if (open) {
+			LeaderboardRanks.gameObject.SetActive(true);
+			LeaderboardNames.gameObject.SetActive(true);
+			LeaderboardScores.gameObject.SetActive(true);
+
+			char[] spliiterChar = { '\r', '\n' };
+
+			//Do some mad shit with typing things out
+			string[] ranks = LeaderboardRanks.text.Split(spliiterChar,10);
+			string[] names = LeaderboardNames.text.Split(spliiterChar,10);
+			string[] scores = LeaderboardScores.text.Split(spliiterChar,10);
+
+			string _ranks = LeaderboardRanks.text;
+			string _names = LeaderboardNames.text;
+			string _scores = LeaderboardScores.text;
+
+			LeaderboardScores.text = "";
+			LeaderboardNames.text = "";
+			LeaderboardRanks.text = "";
+
+			#region method A
+			/*for (int i = 0; i < 3; i++) {
+				
+				LeaderboardRanks.text = "";
+				foreach (string s in ranks) {
+					if (i < s.Length)
+						LeaderboardRanks.text += s.Substring(0,i) + '\n';
+					else
+						LeaderboardRanks.text += s + '\n';
+				}
+				yield return new WaitForEndOfFrame ();
+				yield return new WaitForEndOfFrame ();
+				yield return new WaitForEndOfFrame ();
+			}
+
+			for (int i = 0; i < 7; i++) {
+				
+				LeaderboardNames.text = "";
+				foreach (string s in names) {
+					if (i < s.Length)
+						LeaderboardNames.text += s.Substring(0,i) + '\n';
+					else
+						LeaderboardNames.text += s + '\n';
+				}
+
+				yield return new WaitForEndOfFrame ();
+				yield return new WaitForEndOfFrame ();
+				yield return new WaitForEndOfFrame ();
+			}
+
+			for (int i = 0; i < 10; i++) {
+				LeaderboardScores.text = "";
+				foreach (string s in scores) {
+					if (i < s.Length)
+						LeaderboardScores.text += s.Substring(0,i) + '\n';
+					else
+						LeaderboardScores.text += s + '\n';
+				}
+				yield return new WaitForEndOfFrame ();
+				yield return new WaitForEndOfFrame ();
+				yield return new WaitForEndOfFrame ();
+			}*/
+			#endregion
+
+			#region method B
+			for (int i = 0; i < _ranks.Length; i++) {
+				LeaderboardRanks.text += _ranks [i];
+				yield return new WaitForEndOfFrame ();
+			}
+			for (int i = 0; i < _names.Length; i++) {
+				LeaderboardNames.text += _names [i];
+				yield return new WaitForEndOfFrame ();
+			}
+			for (int i = 0; i < _scores.Length; i++) {
+				LeaderboardScores.text += _scores [i];
+				yield return new WaitForEndOfFrame ();
+			}
+			#endregion
+
+			#region method C
+			//Same as method B, only do a line at a time
+			for (int i = 0; i < _ranks.Length; i++) {
+				LeaderboardRanks.text += _ranks [i];
+				yield return new WaitForEndOfFrame ();
+			}
+			for (int i = 0; i < _names.Length; i++) {
+				LeaderboardNames.text += _names [i];
+				yield return new WaitForEndOfFrame ();
+			}
+			for (int i = 0; i < _scores.Length; i++) {
+				LeaderboardScores.text += _scores [i];
+				yield return new WaitForEndOfFrame ();
+			}
+			#endregion
+		}
+		state = open ? MenuState.leaderboards : MenuState.main;
 	}
 
 	IEnumerator StartGame ()
